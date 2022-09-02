257f170764188eaa65cb28267dc834de18ce4757
Brandon Riddle <brandonriddle828@gmail.com>
2022-09-01 20:41:58 -0400

    Question3

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -10,8 +10,9 @@
         Console.ReadLine();
         Console.Clear();
 
-        program.Question1();
-        program.Question2();
+       // program.Question1();
+       // program.Question2();
+        program.Question3();
 
     }
     
@@ -43,6 +44,48 @@
         Console.WriteLine("Press ENTER to move to the next question.");
         Console.ReadLine();
         Console.Clear();
+
+    }
+
+    public void Question3()
+    {
+
+        string[] names = 
+        { 
+            "sbyte", "byte", "short", "ushort",
+            "int", "uint", "long", "ulong", "float", "double", "decimal" 
+        };
+
+
+        int[] size = 
+        { 
+            sizeof(sbyte), sizeof(byte), sizeof(short), sizeof(ushort), sizeof(int), 
+            sizeof(uint), sizeof(long), sizeof(ulong), sizeof(float),sizeof(double),sizeof(decimal) 
+        };
+
+        string [] min =
+        {
+            sbyte.MinValue.ToString(), byte.MinValue.ToString(), short.MinValue.ToString(),  ushort.MinValue.ToString(), 
+            int.MinValue.ToString(), uint.MinValue.ToString(), long.MinValue.ToString(), ulong.MinValue.ToString(), float.MinValue.ToString(), 
+            double.MinValue.ToString(), decimal.MinValue.ToString()
+        };
+
+        string[] max=
+{
+            sbyte.MaxValue.ToString(), byte.MaxValue.ToString(), short.MaxValue.ToString(),  ushort.MaxValue.ToString(),
+            int.MaxValue.ToString(), uint.MaxValue.ToString(), long.MaxValue.ToString(), ulong.MaxValue.ToString(), float.MaxValue.ToString(),
+            double.MaxValue.ToString(), decimal.MaxValue.ToString()
+        };
+
+
+        Console.WriteLine("===========================================================================================================");
+        Console.WriteLine("{0,-20} {1,5} {2,30} {3,39}\n", "Type", "Bytes in memory", "Min", "Max");
+        Console.WriteLine("===========================================================================================================");
+        for (int i = 0; i < names.Length; i++)
+            Console.WriteLine("{0,-20} {1,5:N1} {2,40}{3,40}", names[i], size[i], min[i], max[i]);
+
+
+
 
     }
 }
