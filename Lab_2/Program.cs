ac81a872c7381085439730bb6c56aa4ba0bb4cc5
Brandon Riddle <brandonriddle828@gmail.com>
2022-09-03 11:55:04 -0400

    Reformatted

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -20,9 +20,9 @@
     public void Question1 ()
     {
         Console.WriteLine("First we are going to add two numbers!");
-        Console.WriteLine("Please type the first number");
+        Console.WriteLine("Please type the first number.");
         double a = double.Parse(Console.ReadLine());
-        Console.WriteLine("Great! Now type the second number!");
+        Console.WriteLine("Great! Now type the second number.");
         double b = double.Parse(Console.ReadLine());
         Console.WriteLine($"Thank you! The sum of {a} + {b} = {a + b}");
         Console.WriteLine();
@@ -36,9 +36,9 @@
     public void Question2()
     {
         Console.WriteLine("Now we are going to multiply two numbers!");
-        Console.WriteLine("Please type the first number");
+        Console.WriteLine("Please type the first number.");
         double c = double.Parse(Console.ReadLine());
-        Console.WriteLine("Great! Now type the second number!");
+        Console.WriteLine("Great! Now type the second number.");
         double d = double.Parse(Console.ReadLine());
         Console.WriteLine($"Thank you! The sum of {c} * {d} = {c * d}");
         Console.WriteLine();
