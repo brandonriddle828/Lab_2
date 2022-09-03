02945da19d93c40919d527de9a38ce4668431576
Brandon Riddle <brandonriddle828@gmail.com>
2022-09-03 11:45:50 -0400

    question4

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -6,13 +6,14 @@
     {
         Program program = new Program();
         Console.WriteLine("Welcome to the Lab 2 assignment. " +
-            "Press ENTER to start the program!");
+            "Press any key to start the program!");
         Console.ReadLine();
         Console.Clear();
 
        // program.Question1();
        // program.Question2();
-        program.Question3();
+        //program.Question3();
+        program.Question4();
 
     }
     
@@ -25,7 +26,7 @@
         double b = double.Parse(Console.ReadLine());
         Console.WriteLine($"Thank you! The sum of {a} + {b} = {a + b}");
         Console.WriteLine();
-        Console.WriteLine("Press ENTER to move to the next question.");
+        Console.WriteLine("Press any key to move to the next question.");
         Console.ReadLine();
         Console.Clear();
     }
@@ -41,7 +42,7 @@
         double d = double.Parse(Console.ReadLine());
         Console.WriteLine($"Thank you! The sum of {c} * {d} = {c * d}");
         Console.WriteLine();
-        Console.WriteLine("Press ENTER to move to the next question.");
+        Console.WriteLine("Press any key to move to the next question.");
         Console.ReadLine();
         Console.Clear();
 
@@ -82,10 +83,175 @@
         Console.WriteLine("{0,-20} {1,5} {2,30} {3,39}\n", "Type", "Bytes in memory", "Min", "Max");
         Console.WriteLine("===========================================================================================================");
         for (int i = 0; i < names.Length; i++)
-            Console.WriteLine("{0,-20} {1,5:N1} {2,40}{3,40}", names[i], size[i], min[i], max[i]);
+        Console.WriteLine("{0,-20} {1,5} {2,40}{3,40}", names[i], size[i], min[i], max[i]);
+        Console.WriteLine("===========================================================================================================");
+
+    }
+
+    public void Question4()
+    {
+        Console.WriteLine("This is the final question on the assignment.\n" +
+            "Here we have created a 5 function calculator!");
+        Console.WriteLine();
+        Console.WriteLine("Please press any key To Start");
+        Console.ReadLine();
+        Console.Clear();
+
+
+            Console.WriteLine("Please type the first number you want to start with!");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Now type the operator sign you wish to use. Example ( -, +, *, /, or % )");
+            string y = Console.ReadLine();
+            Console.WriteLine("Now type the last number you wish to use!");
+            double z = double.Parse(Console.ReadLine());
+            double result=0;
+
+        
+            if (y == "+")
+            {
+                Console.WriteLine("Great! So...");
+                result = x + z;
+                Console.WriteLine($"{x}{y}{z} = {result}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+             
+
+            }
+            else if (y == "-")
+            {
+                Console.WriteLine("Great! So...");
+                result = x - z;
+                Console.WriteLine($"{x}{y}{z} = {result}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+            }
+            else if (y == "*")
+            {
+                Console.WriteLine("Great! So...");
+                result = x * z;
+                Console.WriteLine($"{x}{y}{z} = {result}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+            }
+            else if (y == "/")
+            {
+                Console.WriteLine("Great! So...");
+                result = x / z;
+                Console.WriteLine($"{x}{y}{z} = {result}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+             
+            }
+            else if (y == "%")
+            {
+                Console.WriteLine("Great! So...");
+                result = x % z;
+                Console.WriteLine($"{x}{y}{z} = {result}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+              
+            }
+            if(Console.ReadKey().Key != ConsoleKey.Escape)
+
+        do
+        {
+            Console.WriteLine($"Your result is {result} \nWhat would you like to do next to this number?");
+            Console.WriteLine($"Type sign you wish to use. Ex (-,+,*,/,%)");
+            string sign = Console.ReadLine();
+            Console.WriteLine("Now choose the next number you wish to use!");
+            double nextNumber = double.Parse(Console.ReadLine());
 
 
 
+            if (sign == "+")
+            {
+                Console.WriteLine("Great! So...");
+                Console.WriteLine($"{result} + {nextNumber} = {result = result + nextNumber}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+
+
+            }
+            else if (sign == "-")
+            {
+                Console.WriteLine("Great! So...");
+                Console.WriteLine($"{result} - {nextNumber} = {result = result - nextNumber}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+            }
+            else if (sign == "*")
+            {
+                Console.WriteLine("Great! So...");
+                Console.WriteLine($"{result} * {nextNumber} = {result = result * nextNumber}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+            }
+            else if (sign == "/")
+            {
+                Console.WriteLine("Great! So...");
+                Console.WriteLine($"{result} / {nextNumber} = {result = result / nextNumber}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+
+            }
+            else if (sign == "%")
+            {
+                Console.WriteLine("Great! So...");
+                Console.WriteLine($"{result} % {nextNumber} = {result = result % nextNumber}");
+
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to use the calculator again?\n" +
+                    "No:Press 'esc' \n" +
+                    "Yes:Press any other key");
+                Console.WriteLine();
+               
+
+            }
+        }
+        
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
 
     }
 }
