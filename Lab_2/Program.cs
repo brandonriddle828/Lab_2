510187162fc20a52453bf6b7e677dbce0a002301
Brandon Riddle <brandonriddle828@gmail.com>
2022-09-03 12:50:25 -0400

    try/catch fix

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -10,46 +10,80 @@
         Console.ReadLine();
         Console.Clear();
 
-        program.Question1();
-        program.Question2();
-        program.Question3();
+        //program.Question1();
+        //program.Question2();
+        //program.Question3();
         program.Question4();
 
     }
-    
-    public void Question1 ()
+
+    public void Question1()
     {
-        Console.WriteLine("First we are going to add two numbers!");
-        Console.WriteLine("Please type the first number.");
-        double a = double.Parse(Console.ReadLine());
-        Console.WriteLine("Great! Now type the second number.");
-        double b = double.Parse(Console.ReadLine());
-        Console.WriteLine($"Thank you! The sum of {a} + {b} = {a + b}");
-        Console.WriteLine();
-        Console.WriteLine("Press 'Enter' to move to the next question.");
-        Console.ReadLine();
-        Console.Clear();
+        bool flag = false;
+        do
+        {
+            try
+            {
+                Console.WriteLine("First we are going to add two numbers!");
+                Console.WriteLine("Please type the first number.");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Great! Now type the second number.");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Thank you! The sum of {a} + {b} = {a + b}");
+                Console.WriteLine();
+                Console.WriteLine("Press 'Enter' to move to the next question.");
+                Console.ReadLine();
+                Console.Clear();
+                flag = true;
+            }
+            catch (Exception e)
+            {
+                flag = false;
+                Console.WriteLine("You have entered something wrong, please try again.");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        while (flag == false);
     }
 
 
 
     public void Question2()
     {
-        Console.WriteLine("Now we are going to multiply two numbers!");
-        Console.WriteLine("Please type the first number.");
-        double c = double.Parse(Console.ReadLine());
-        Console.WriteLine("Great! Now type the second number.");
-        double d = double.Parse(Console.ReadLine());
-        Console.WriteLine($"Thank you! The sum of {c} * {d} = {c * d}");
-        Console.WriteLine();
-        Console.WriteLine("Press 'Enter' to move to the next question.");
-        Console.ReadLine();
-        Console.Clear();
+        bool flag = false;
+        do
+        {
+            try
+            {
+                Console.WriteLine("Now we are going to multiply two numbers!");
+                Console.WriteLine("Please type the first number.");
+                double c = double.Parse(Console.ReadLine());
+                Console.WriteLine("Great! Now type the second number.");
+                double d = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Thank you! The sum of {c} * {d} = {c * d}");
+                Console.WriteLine();
+                Console.WriteLine("Press 'Enter' to move to the next question.");
+                Console.ReadLine();
+                Console.Clear();
+                flag = true;
+            }
+
+            catch (Exception e)
+            {
+                flag = false;
+                Console.WriteLine("You have entered something wrong, please try again.");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        while (flag == false);
 
     }
 
     public void Question3()
     {
+
         Console.WriteLine("This is the 3rd question.");
         Console.WriteLine("Here is neatly printed list of data types.");
         Console.WriteLine();
@@ -97,168 +131,230 @@
 
     public void Question4()
     {
-        Console.WriteLine("This is the final question on the assignment.\n" +
-            "Here we have created a 5 function calculator!");
-        Console.WriteLine();
-        Console.WriteLine("Please press 'Enter' To Start");
-        Console.ReadLine();
-        Console.Clear();
-
-
-            Console.WriteLine("Please type the first number you want to start with!");
-            double x = double.Parse(Console.ReadLine());
-            Console.WriteLine("Now type the operator sign you wish to use. Example ( -, +, *, /, or % )");
-            string y = Console.ReadLine();
-            Console.WriteLine("Now type the last number you wish to use!");
-            double z = double.Parse(Console.ReadLine());
-            double result=0;
-
-        
-            if (y == "+")
-            {
-                Console.WriteLine("Great! So...");
-                result = x + z;
-                Console.WriteLine($"{x}{y}{z} = {result}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-             
-
-            }
-            else if (y == "-")
-            {
-                Console.WriteLine("Great! So...");
-                result = x - z;
-                Console.WriteLine($"{x}{y}{z} = {result}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-               
-            }
-            else if (y == "*")
-            {
-                Console.WriteLine("Great! So...");
-                result = x * z;
-                Console.WriteLine($"{x}{y}{z} = {result}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-               
-            }
-            else if (y == "/")
-            {
-                Console.WriteLine("Great! So...");
-                result = x / z;
-                Console.WriteLine($"{x}{y}{z} = {result}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-             
-            }
-            else if (y == "%")
-            {
-                Console.WriteLine("Great! So...");
-                result = x % z;
-                Console.WriteLine($"{x}{y}{z} = {result}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-              
-            }
-            if(Console.ReadKey().Key != ConsoleKey.Escape)
-
+       
+        bool flag = false;
         do
         {
-            Console.WriteLine($"Your previous result is {result} \nWhat would you like to do next to this number?");
-            Console.WriteLine($"Type the operator sign you wish to use. Ex (-,+,*,/,%)");
-            string sign = Console.ReadLine();
-            Console.WriteLine("Now choose the next number you wish to use!");
-            double nextNumber = double.Parse(Console.ReadLine());
-
-
-
-            if (sign == "+")
+            try
             {
-                Console.WriteLine("Great! So...");
-                Console.WriteLine($"{result} + {nextNumber} = {result = result + nextNumber}");
+                Console.Clear();
+                flag = true;
+                Console.WriteLine("This is the final question on the assignment.\n" +
+                    "Here we have created a 5 function calculator!");
+                Console.WriteLine();
+                Console.WriteLine("Please press 'Enter' To Start");
+                Console.ReadLine();
+                Console.Clear();
 
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
+
+                Console.WriteLine("Please type the first number you want to start with!");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Now type the operator sign you wish to use. Example ( -, +, *, /, or % )");
+                string y = Console.ReadLine();
+                Console.WriteLine("Now type the last number you wish to use!");
+                double z = double.Parse(Console.ReadLine());
+                double result=0;
+
+                switch(y)
+                {
+                    case "+":
+                        {
+                            Console.WriteLine("Great! So...");
+                            result = x + z;
+                            Console.WriteLine($"{x}{y}{z} = {result}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+
+                        break;
+
+                        case "-":
+                        {
+                            Console.WriteLine("Great! So...");
+                            result = x - z;
+                            Console.WriteLine($"{x}{y}{z} = {result}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+
+                        break;
+
+                    case "*":
+                        {
+                            Console.WriteLine("Great! So...");
+                            result = x * z;
+                            Console.WriteLine($"{x}{y}{z} = {result}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+
+                        break;
+
+                    case "/":
+                        {
+                            Console.WriteLine("Great! So...");
+                            result = x / z;
+                            Console.WriteLine($"{x}{y}{z} = {result}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+
+                        break;
+
+                    case "%":
+                        {
+                            Console.WriteLine("Great! So...");
+                            result = x % z;
+                            Console.WriteLine($"{x}{y}{z} = {result}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+                        break;
+
+                        default:
+                        {
+                                        
+                            flag = false;
+                            Console.WriteLine("You have entered something wrong, please press enter and try again.");
+                            Console.WriteLine();
+                            Console.WriteLine();
+
+                        }
+                        break;
+                }
+
+
+
+                if (Console.ReadKey().Key != ConsoleKey.Escape && flag==true)
+                
+
+                    do
+                    {
+                        Console.WriteLine($"Your previous result is {result} \nWhat would you like to do next to this number?");
+                        Console.WriteLine($"Type the operator sign you wish to use. Ex (-,+,*,/,%)");
+                        string sign = Console.ReadLine();
+                        Console.WriteLine("Now choose the next number you wish to use!");
+                        double nextNumber = double.Parse(Console.ReadLine());
+
+
+
+                        if (sign == "+")
+                        {
+                            Console.WriteLine("Great! So...");
+                            Console.WriteLine($"{result} + {nextNumber} = {result = result + nextNumber}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
                
 
 
+                        }
+                        else if (sign == "-")
+                        {
+                            Console.WriteLine("Great! So...");
+                            Console.WriteLine($"{result} - {nextNumber} = {result = result - nextNumber}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+               
+                        }
+                        else if (sign == "*")
+                        {
+                            Console.WriteLine("Great! So...");
+                            Console.WriteLine($"{result} * {nextNumber} = {result = result * nextNumber}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+               
+                        }
+                        else if (sign == "/")
+                        {
+                            Console.WriteLine("Great! So...");
+                            Console.WriteLine($"{result} / {nextNumber} = {result = result / nextNumber}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+               
+
+                        }
+                        else if (sign == "%")
+                        {
+                            Console.WriteLine("Great! So...");
+                            Console.WriteLine($"{result} % {nextNumber} = {result = result % nextNumber}");
+
+                            Console.WriteLine();
+                            Console.WriteLine("Do you wish to use the calculator again?\n" +
+                                "No:Press 'esc' \n" +
+                                "Yes:Press any other key");
+                            Console.WriteLine();
+
+                        }
+
+                        else
+                        {
+                            flag = false;
+                            Console.WriteLine("You have entered something wrong, please press enter and try again.");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                      
+                            
+                        }
+
+                    }
+        
+                    while (Console.ReadKey().Key != ConsoleKey.Escape && flag == true);
+                   
             }
-            else if (sign == "-")
+
+            catch (Exception e)
             {
-                Console.WriteLine("Great! So...");
-                Console.WriteLine($"{result} - {nextNumber} = {result = result - nextNumber}");
-
+                flag = false;
+                Console.WriteLine("You have entered something wrong, please press enter and try again.");
                 Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
                 Console.WriteLine();
-               
-            }
-            else if (sign == "*")
-            {
-                Console.WriteLine("Great! So...");
-                Console.WriteLine($"{result} * {nextNumber} = {result = result * nextNumber}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-               
-            }
-            else if (sign == "/")
-            {
-                Console.WriteLine("Great! So...");
-                Console.WriteLine($"{result} / {nextNumber} = {result = result / nextNumber}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-               
-
-            }
-            else if (sign == "%")
-            {
-                Console.WriteLine("Great! So...");
-                Console.WriteLine($"{result} % {nextNumber} = {result = result % nextNumber}");
-
-                Console.WriteLine();
-                Console.WriteLine("Do you wish to use the calculator again?\n" +
-                    "No:Press 'esc' \n" +
-                    "Yes:Press any other key");
-                Console.WriteLine();
-               
-
+                
+                Console.Clear();
             }
         }
-        
-        while (Console.ReadKey().Key != ConsoleKey.Escape);
+        while (flag == false) ;
 
     }
+   
 }
